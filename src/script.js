ad7bf0a43423fe098fbe049db1c0c46a858ef2a9
akshayachu407 <akshay.g.b.e@gmail.com>
2022-07-06 21:26:21 -0700

    added new js

--- src/script.js
+++ src/script.js
@@ -1,2 +1,20 @@
-var names=document.getElementById('name');
-console.log(names);
+
+
+
+function getValues(){
+    var names=document.getElementById('name');
+    var name= "akshay";
+    window.alert(name);
+
+}
+function newValues(){
+    var name="thilak";
+    console.log(name);
+}
+function setValues(){
+    var name="gayu";
+    console.log(name);
+}
+function newData(){
+   console.log("getlat") ;
+}
