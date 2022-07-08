8f94c964218a299623a666bf10f358088e49eac5
akshayachu407 <akshay.g.b.e@gmail.com>
2022-07-08 06:02:48 -0700

    added full changes

--- src/script.js
+++ src/script.js
@@ -5,16 +5,22 @@ function getValues(){
     var names=document.getElementById('name');
     var name= "akshay";
     window.alert(name);
-
+    newValues();
 }
+
 function newValues(){
     var name="thilak";
     console.log(name);
+    setValues();
 }
+
 function setValues(){
     var name="gayu";
     console.log(name);
+    newData();
 }
+
 function newData(){
    console.log("getlat") ;
+   
 }
